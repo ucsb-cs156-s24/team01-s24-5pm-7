65e30510b587de2aae3ded91758bbccc55976442
katelarrick <katelarrick@umail.ucsb.edu>
2024-04-18 16:35:58 -0700

    created test for location query service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/LocationQueryService.java
@@ -16,6 +16,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
 import org.springframework.web.client.HttpClientErrorException;
 
 /* GOAL: Location look up (enter a string, get back locations in the world along with their latitude/longitude) */
